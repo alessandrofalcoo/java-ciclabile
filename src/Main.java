6e9a2e50d219563d52c0aba74325d4240c874069
alessandrofalcoo <alefalco.1003@gmail.com>
2025-07-09 11:48:44 +0200

    Add elements to Integers instance in Main class

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int[] numeri = { 10, 20, 30, 40 };
         Integers ints = new Integers(numeri);
+        ints.addElemento(25);
+        ints.addElemento(393);
+        ints.addElemento(248);
         System.out.println(ints);
     }
 }
