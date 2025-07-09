dbc313c098b72e468a3c3fbbd2c98a20fb2b8b54
alessandrofalcoo <alefalco.1003@gmail.com>
2025-07-09 11:30:11 +0200

    Remove commented-out methods from Integers class

--- src/Integers.java
+++ src/Integers.java
@@ -1,22 +1,5 @@
 public class Integers {
     int[] integers = { 1, 2, 4, 6, 28, 29, 40 };
-
-    /*
-     * public int getElementoSuccessivo(int value) {
-     * for (int i = 0; i < integers.length - 1; i++) {
-     * if (integers[i] == value) {
-     * return integers[i + 1];
-     * }
-     * }
-     * throw new IllegalArgumentException("Element not found");
-     * }
-     * 
-     * public boolean hasAncoraElementi(){
-     * if(){
-     * 
-     * }
-     * }
-     */
     private int indiceCorrente = 0;
 
     public int getElementoSuccessivo() {
