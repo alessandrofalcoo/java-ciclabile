030a270c0b29b2c6d70ec6acc367620e3e03978c
alessandrofalcoo <alefalco.1003@gmail.com>
2025-07-09 11:29:09 +0200

    Refactor Integers class methods and update Main class to iterate through elements

--- src/Integers.java
+++ src/Integers.java
@@ -1,13 +1,35 @@
 public class Integers {
     int[] integers = { 1, 2, 4, 6, 28, 29, 40 };
 
-    public int getElementoSuccessivo(int value) {
-        for (int i = 0; i < integers.length; i++) {
-            if (integers[i] == value) {
-                return integers[i + 1];
-            }
+    /*
+     * public int getElementoSuccessivo(int value) {
+     * for (int i = 0; i < integers.length - 1; i++) {
+     * if (integers[i] == value) {
+     * return integers[i + 1];
+     * }
+     * }
+     * throw new IllegalArgumentException("Element not found");
+     * }
+     * 
+     * public boolean hasAncoraElementi(){
+     * if(){
+     * 
+     * }
+     * }
+     */
+    private int indiceCorrente = 0;
+
+    public int getElementoSuccessivo() {
+        if (indiceCorrente < integers.length - 1) {
+            indiceCorrente++;
+            return integers[indiceCorrente];
+        } else {
+            throw new IllegalStateException("Sei già all'ultimo elemento");
         }
-        throw new IllegalArgumentException("Element not found");
+    }
+
+    public boolean hasAncoraElementi() {
+        return indiceCorrente < integers.length - 1;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Integers ints = new Integers();
-        int successivo = ints.getElementoSuccessivo(6);
-        System.out.println("Il successivo di 6 è: " + successivo);
+        while (ints.hasAncoraElementi()) {
+            System.out.println(ints.getElementoSuccessivo());
+        }
     }
 }
